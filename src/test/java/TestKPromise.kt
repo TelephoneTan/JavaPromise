2038c1cea84b729c6fafdb0a8910557442a6c923
Telephone <telephone.tan@qq.com>
2024-04-04 06:06:57 +0800

    添加 KPromise + KPromiseSemaphore 测试代码

--- src/test/java/TestKPromise.kt
+++ src/test/java/TestKPromise.kt
@@ -1,8 +1,9 @@
 import kotlinx.coroutines.*
 import org.junit.jupiter.api.Test
-import pub.telephone.javapromise.async.kpromise.process
-import pub.telephone.javapromise.async.kpromise.promise
+import pub.telephone.javapromise.async.kpromise.*
+import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.coroutines.coroutineContext
 import kotlin.time.Duration.Companion.seconds
 
@@ -139,6 +140,109 @@ class TestKPromise {
                 println("【${time}】$value")
             }.await()
             rsv(null)
+        }.await()
+    }
+
+    @Test
+    fun testSemaphore() {
+        testSemaphoreN(1)
+        testSemaphoreN(5)
+    }
+
+    fun testSemaphoreN(n: Int) {
+        process {
+            val semaphore = PromiseSemaphore(n)
+            val counter = AtomicInteger()
+            val allWork = mutableListOf<Promise<Any?>>()
+            val skip = arrayOf(55, 66, 77)
+            for (i in 1..100) {
+                allWork.add(promise(PromiseConfig(
+                        semaphore = semaphore
+                )) {
+                    if (i in skip) {
+                        throw Throwable("$i 不干了")
+                    }
+                    println("$i 开始值 ${counter.get()}")
+                    for (j in 1..1000000) {
+                        counter.incrementAndGet()
+                    }
+                    println("$i 结束值 ${counter.get()}")
+                    rsv(null)
+                })
+            }
+            promise {
+                var allSucceeded = true
+                for ((i, p) in allWork.withIndex()) {
+                    try {
+                        p.await()
+                    } catch (e: CancellationException) {
+                        allSucceeded = false
+                        println("$i 取消了")
+                    } catch (e: Throwable) {
+                        allSucceeded = false
+                        println("$i 出错了：$e")
+                    }
+                }
+                if (allSucceeded) {
+                    println("全部成功")
+                }
+                rsv(null)
+            }.last {
+                println("总体结束值 ${counter.get()}")
+            }
+        }.await()
+    }
+
+    @Test
+    fun testMultipleSemaphore() {
+        val total: PromiseSemaphore
+        val host: PromiseSemaphore
+        val hostA: PromiseSemaphore
+        val hostB: PromiseSemaphore
+        val user: PromiseSemaphore
+        val userA: PromiseSemaphore
+        val userB: PromiseSemaphore
+        PromiseSemaphore(10).also { total = it }.apply {
+            then(PromiseSemaphore(5).also { host = it }.apply {
+                then(PromiseSemaphore(3).also { hostA = it })
+                then(PromiseSemaphore(3).also { hostB = it })
+            })
+            then(PromiseSemaphore(5).also { user = it }.apply {
+                then(PromiseSemaphore(3).also { userA = it })
+                then(PromiseSemaphore(3).also { userB = it })
+            })
+        }
+        val interval = 5.seconds
+        process {
+            val allWork = mutableListOf<Promise<Any?>>()
+            val add = { name: String, semaphore: PromiseSemaphore ->
+                for (i in 1..10) {
+                    allWork.add(promise(PromiseConfig(semaphore = semaphore)) {
+                        println("$name $i -> ${
+                            SimpleDateFormat(
+                                    "yyyy / MM / dd | HH : mm : ss . SSS"
+                            ).format(Date())
+                        }")
+                        delay(interval)
+                        rsv(null)
+                    })
+                }
+            }
+            add("hostA", hostA)
+            add("hostB", hostB)
+            add("userA", userA)
+            add("userB", userB)
+            promise {
+                for (p in allWork) {
+                    try {
+                        p.await()
+                    } catch (_: Throwable) {
+                    }
+                }
+                rsv(null)
+            }.last {
+                println("结束了")
+            }
         }.await()
     }
 
